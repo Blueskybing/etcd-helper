8529d86054ddbde58142377f9af5b6b77ab29904
Bluesky <huang.bx@yuwan8.com>
2015-12-04 13:04:48 +0800

    修复bug

--- test/test.js
+++ test/test.js
@@ -1,23 +1,28 @@
 /*
-var Etcd = require("../lib/etcd-helper");
-var params = {
-    mysql: [{
-        path: '/yuwan/mysql/development/master/',
-        config: {},
-        db_pool: {}
-    }],
-    redis: {
-        path: '/yuwan/redis/development/',
-        config: {}
-    }
-};
-new Etcd(['xxx.xxx.xxx.xxx:xxxx', 'xxx.xxx.xxx.xxx:xxxx', 'xxx.xxx.xxx.xxx:xxxx']).setMysqlConfig(params);
+ var Etcd = require("../lib/etcd-helper");
+ var params = {
+ mysql: [{
+ path: '/yuwan/mysql/development/master/',
+ config: {},
+ db_pool: {}
+ }],
+ redis: {
+ path: '/yuwan/redis/development/',
+ config: {}
+ }
+ };
+ new Etcd(['xxx.xxx.xxx.xxx:xxxx', 'xxx.xxx.xxx.xxx:xxxx', 'xxx.xxx.xxx.xxx:xxxx']).setMysqlConfig(params);
 
 
-console.log(params.mysql[0].config);
-console.log(params);
-*/
+ console.log(params.mysql[0].config);
+ console.log(params);
+ */
 
-var a = 'sd';
+/*
+ var a = 'sd';
 
-console.log(a.indexOf('a'))
+ console.log(a.indexOf('a'))*/
+
+var a = {'a': 1}
+
+console.log(typeof a === 'object')
